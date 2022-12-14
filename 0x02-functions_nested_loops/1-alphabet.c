357a1a345e6937f2d89e9a3f3eb874848d2a9eff
Frazier-Creation <ochiengfrazier@gmail.com>
2022-12-14 08:39:15 -0800

    I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabrt - Make the aplhabet
+ *
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
