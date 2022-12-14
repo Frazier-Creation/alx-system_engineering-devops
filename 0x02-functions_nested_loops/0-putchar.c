6ff94b63407c8c658646e2f30b8a75f8b70c479d
Frazier-Creation <ochiengfrazier@gmail.com>
2022-12-14 10:39:27 -0800

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,19 +3,32 @@
 /**
  * main - print _putchar
  *
+ * is written by frazier
+ *
  * Return: 0
+ *
  */
 
 int main(void)
+
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+
+	char frazier[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
+	int b = 0;
+
+	while (b < 9)
+
+	{
+
+		_putchar(frazier[b]);
+
+		b  =  b + 1;
+
+	}
+
 	_putchar('\n');
+
 	return (0);
+
 }
